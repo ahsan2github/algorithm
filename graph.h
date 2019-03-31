16ce733447730b0a0580669b76ee6726a9c78ed6
Ahsan Khan <ahsan.mecha@gmail.com>
2019-03-31 17:26:07 -0600

    Add dfs and graph

---
+++ graph.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+class Graph{
+public:
+    Graph(int v)
+    Graph(In in)
+    addEdge(int u, int v)
+    adj(int v)
+    int getEdgeNos()
+    int getVertexNos()
+    std::string toString()
+};
